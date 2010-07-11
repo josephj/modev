2f83fd845e0a006cdaac9ac66547f13c659cc70c
Joseph Chiang <josephj6802@gmail.com>
2010-07-11 12:41:36 -0400

    remove old files

--- static/framework/sandbox.js
+++
@@ -1,9 +0,0 @@
-// sandbox : module api interface, security guard
-Y.add("sandbox", function (Y) {
-    var Sandbox = function (node) {
-        this.node = node;
-    };
-    Sandbox.prototype.one = function (query) {
-        return this.node.one(query);
-    };
-});
