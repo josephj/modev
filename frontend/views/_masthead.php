502068f06f7764a03e66765327e1e07b3d3f3625
Joseph Chiang <josephj6802@gmail.com>
2010-07-15 11:30:51 -0400

    do something right

--- frontend/views/_masthead.php
+++ frontend/views/_masthead.php
@@ -3,12 +3,12 @@
     <div class="bd clearfix">
         <span class="title">    
             <a name="top">
-                <img src="http://a.mimgs.com/index/logo_156x43.png" width="156" height="43">
+                <img src="<?php echo STATIC_URL; ?>logo_156x43.png" width="156" height="43">
                 Modular Development
             </a>
         </span>
         <a href="http://developer.yahoo.com/yui/3" class="extra" target="_blank">
-            <img src="http://josephj.com/training/nctu/ico_yui.gif" alt="Powered By YUI" width="80" height="40">
+            <img src="<?php echo STATIC_URL; ?>ico_yui.gif" alt="Powered By YUI" width="80" height="40">
         </a>
     </div>
 </div>
