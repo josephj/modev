c70d1b4e247f64ec66eb2dd7ec7de222c1764908
Joseph Chiang <josephj6802@gmail.com>
2010-07-20 05:57:23 -0400

    coding style fix and documentation

--- static/_photo_filter.js
+++ static/_photo_filter.js
@@ -1,61 +1,89 @@
-Y.Core.register("photo-filter", function() {
-    var _api;
-    return {
-        /* 
-         * 模块初始化
-         * @method init
-         */
-        init: function (api) {
-            this.api = api;
-            _api = this.api;
-        },
-        /* 
-         * 模块内容读取完毕 (YUI onContentReady)
-         * @method onviewload
-         * @return void
-         */
-        onviewload: function () {
-            this.node = this.api.getViewNode();
-            this.node.one("form").on("submit", this._formSubmitHandler, this);
-            if (this.node.one("form .keyword").get("value")) {
-                this._makeRequest();    
-            }
-        },
-        /* 
-         * 更新按钮点选事件
-         * @event _linkClickHandler
-         * @param e {Y.Event} Event Object
-         */
-        _formSubmitHandler: function (e) {
-            e.preventDefault();
-            this.api.broadcast("photo-filter-submit");
-            this._makeRequest();    
-        },
-        _makeRequest: function () {
-            var attrs    = {},                       // 用于 url 字串取代
-                callback = "",                       // Flickr 回传资料的全域变量名称
-                url      = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=d498ec869768ecea276a7cb3906241d9&tags={tags}&per_page=100&sort=interestingness-desc&format=json&jsoncallback={callback}";  // Flickr API 网址
+/*global Y, window */
+(function () { 
+    Y.Core.register("photo-filter", function () {
+        var api,
+            node,
+            //=================================
+            // Constants
+            //=================================
+            API_ENTRYPOINT = "http://api.flickr.com/services/rest/",
+            API_METHOD = "flickr.photos.search",
+            API_KEY = "d498ec869768ecea276a7cb3906241d9",
+            API_RESPONSE_FORMAT = "json",
+            //=================================
+            // Private methods
+            //=================================
+            /* 
+             * Data response callback
+             * @method getData
+             * @private
+             * @return void
+             */
+            getData = function (o) {
+                api.broadcast("photo-filter-response", o);
+            },
+            /* 
+             * Make request to get data 
+             * @method makeRequest
+             * @return void
+             */
+            makeRequest = function () {
+                var tokens    = {},
+                    callback = "",
+                    url      = API_ENTRYPOINT + "?method=" + API_METHOD + "&api_key=" + API_KEY + "&tags={tags}&per_page=100&sort=interestingness-desc&format=" + API_RESPONSE_FORMAT + "&jsoncallback={callback}";  // Flickr API 网址
 
-            callback = "_getData_" + parseInt(new Date().getTime(), 10);
-            attrs = {
-                "tags": encodeURIComponent(this.node.one("form .keyword").get("value")),
-                "callback": callback
-            };
-            url = Y.substitute(url, attrs);
-            window[callback] = this._getData;
-            Y.Get.script(url, {
-                onSuccess : function() {
-                    window[callback] = null;
+                callback = "_getData_" + parseInt(new Date().getTime(), 10);
+                tokens = {
+                    "tags": encodeURIComponent(node.one("form .keyword").get("value")),
+                    "callback": callback
+                };
+                url = Y.substitute(url, tokens);
+                window[callback] = getData;
+                Y.Get.script(url, {
+                    onSuccess : function () {
+                        window[callback] = null;
+                    }
+                });
+            },
+            /* 
+             * Form submit event handler
+             * @event formSubmitHandler
+             * @param e {Y.Event} Event Object
+             * @private
+             * @return void
+             */
+            formSubmitHandler = function (e) {
+                e.preventDefault();
+                api.broadcast("photo-filter-submit");
+                makeRequest();    
+            },
+            //=================================
+            // Public methods
+            //=================================
+            /* 
+             * Module initialization
+             * @method init
+             * @param sandbox {Y.Sandbox}
+             */
+            init = function (sandbox) {
+                Y.log("init() is executed.", "info", "#photo-filter");
+                api = sandbox;
+            },
+            /* 
+             * Module content ready event
+             * @method onviewload
+             * @return void
+             */
+            onviewload = function () {
+                node = api.getViewNode();
+                node.one("form").on("submit", formSubmitHandler, this);
+                if (node.one("form .keyword").get("value")) {
+                    makeRequest();    
                 }
-            });
-        },
-        /* 
-         * Flickr API 回传 Callback
-         * @method _getData
-         * @private
-         */
-        _getData: function (o) {
-            _api.broadcast("photo-filter-response", o);
-        }
-    }
+            };
+        return {
+            init: init,
+            onviewload: onviewload
+        };
+    }());
 }());
