28c9179b6b02a4fcd9082efa47d911412a6b6ada
Joseph Chiang <josephj6802@gmail.com>
2010-07-20 05:37:21 -0400

    coding style fix and documentation

--- static/core.js
+++ static/core.js
@@ -2,36 +2,74 @@
 YUI.add("core", function (Y) {
     var registeredModules = [],
         listeners = {},
+        //===========================
+        // Private Functions & Events
+        //===========================
+        /* 
+         * Match event and modules which subscribes the event
+         * @method match
+         * @param eventName {String} Event label name
+         * @param callerId {String} The ID of the module which just broadcasts
+         * @param callerData {Object} The data that a broadcasting module wants to share 
+         * @return void
+         */
         match = function (eventName, callerId, callerData) {
-            var modules = [],
+            Y.log("_match(\"" + eventName + "\", \"" + callerId + "\", \"" + callerData + "\") is executed.", "info", "Core");
+            var modules = [], 
                 i,
                 x;
             for (i in listeners) {
                 for (x in listeners[i]) {
                     if (listeners[i][x] === eventName) {
-                        if (typeof registeredModules[i].onmessage !== "undefined") {
-                            registeredModules[i].onmessage(eventName, callerId, callerData);    
-                            modules.push(i);
+                        // trigger module's onmessage event
+                        if (typeof registeredModules[i].onmessage === "undefined") {
+                            continue;
                         }
+                        registeredModules[i].onmessage(eventName, callerId, callerData);    
+                        modules.push(i);
                     }
                 }    
             }    
-            Y.log("_match() is executed successfully, " + modules.length + " modules are influenced: " + modules.join(","), "info", "Core");
+            Y.log("_match(\"" + eventName + "\", \"" + callerId + "\", \"" + callerData + "\") is executed successfully, " + modules.length + " module(s) is(are) influenced: \"#" + modules.join(", #") + "\"", "info", "Core");
         },
-        addListener = function (id, name) {
-            var i, j;
-            if (typeof listeners[id] === "undefined") {
-                listeners[id] = [];
+        /* 
+         * Let a module listen for a specific message 
+         * @method addListener
+         * @param moduleId {String} ID of the module which wants to listen.
+         * @param msgName {String} Target message label name.
+         * @private
+         * @return {Boolean} false if this module has already listened target message
+         */
+        addListener = function (moduleId, msgName) {
+            Y.log("_addListener(\"" + moduleId + "\", \"" + msgName + "\") is executed.", "info", "Core");
+            var i, 
+                j,
+                listener;
+            if (typeof listeners[moduleId] === "undefined") {
+                listener = listeners[moduleId] = [msgName];
+                //Y.log("_addListener(\"" + moduleId + "\", \"" + msgName + "\") is added successfully", "info", "Core");
+                return true;
             }
-            for (i = 0, j = listeners.length; i < j; i += 1) {
-                if (listeners[id][i] === name) {
+            for (i in listener) {
+                if (listener[i] === msgName) {
+                    Y.log("_addListener(\"" + moduleId + "\", \"" + msgName + "\") has already existed", "info", "Core");
                     return false;
                 }
             }
-            listeners[id].push(name);
+            listeners[moduleId].push(msgName);
+            //Y.log("_addListener(\"" + moduleId + "\", \"" + msgName + "\") is added successfully", "info", "Core");
+            return true;
         },
+        /* 
+         * Register a module to Core
+         * @method register
+         * @param moduleId {String} ID of the module which wants to register.
+         * @param attrs {String} Methods/attributes object which the registering module has.
+         * @public
+         * @return {Boolean} false if target message is registered by this module
+         */
         register = function (moduleId, o) {
-            Y.log("register() initialized.", "info", "Core"); 
+            Y.log("register(\"" + moduleId + "\", " + o + ") is executed.", "info", "Core"); 
             var sandbox;
             registeredModules[moduleId] = o;    
             if (typeof o.init === "undefined") {
@@ -45,6 +83,7 @@ YUI.add("core", function (Y) {
                 return;
             }
             Y.on("contentready", o.onviewload, "#" + moduleId, o);
+            //Y.log("register(\"" + moduleId + "\", \"" + o + "\") is executed successfully.", "info", "Core"); 
         };       
     Y.Core = {
         register: register,
